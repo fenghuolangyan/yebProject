bb9be2567455b83749deccfbf0b0886a9dee4748
xq <779338316@qq.com>
2021-04-27 16:17:51 +0800

    分支提交

--- yeb-server/src/main/java/com/xq/server/vo/adminLoginVo.java
+++ yeb-server/src/main/java/com/xq/server/vo/adminLoginVo.java
@@ -1,6 +1,6 @@
 package com.xq.server.vo;/*
-@author  xq 
-@date 2021/4/10 - 14:23 
+@author  xq  AKIAIOSFODNN7EXAMPLE
+@date 2021/4/10 - 14:23 wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 手握梦想,心向旅行的第10天
 三尺代码 一世天涯
 */
